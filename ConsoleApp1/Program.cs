0a9d1c532ddcd49ffe901d0f940c63b4a4226c4c
R0Y4L45 <106491312+R0Y4L45@users.noreply.github.com>
2022-12-02 16:55:38 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿
+public abstract class Game
+{
+    abstract public void Initialize();
+    abstract public void StartPlay();
+    abstract public void EndPlay();
+
+    //template method
+    public void play()
+    {
+
+        //initialize the game
+        Initialize();
+
+        //start game
+        StartPlay();
+
+        //end game
+        EndPlay();
+    }
+}
+
+
+public class Cricket : Game
+{
+
+    public override void EndPlay()
+    {
+        Console.WriteLine("Cricket Game Finished!");
+    }
+
+
+    public override void Initialize()
+    {
+        Console.WriteLine("Cricket Game Initialized! Start playing.");
+    }
+
+
+    public override void StartPlay()
+    {
+        Console.WriteLine("Cricket Game Started. Enjoy the game!");
+    }
+}
+
+
+public class Football : Game
+{
+
+   public override void EndPlay()
+    {
+        Console.WriteLine("Football Game Finished!");
+    }
+
+    public override void Initialize() 
+    {
+        Console.WriteLine("Football Game Initialized! Start playing.");
+    }
+
+    public override void StartPlay() 
+    {
+        Console.WriteLine("Football Game Started. Enjoy the game!");
+    }
+}
+
+
+public class TemplatePatternDemo
+{
+    public static void Main()
+    {
+
+        Game game = new Cricket();
+        game.play();
+        Console.WriteLine();
+        game = new Football();
+        game.play();
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,110 @@
+﻿public interface IBehaviour
+{
+    public int moveCommand();
+}
+
+public class AgressiveBehaviour : IBehaviour
+{
+
+    public int moveCommand()
+{
+    Console.WriteLine("\tAgressive Behaviour: if find another robot attack it");
+    return 1;
+}
+}
+
+public class DefensiveBehaviour : IBehaviour
+{
+
+    public int moveCommand()
+{
+    Console.WriteLine("\tDefensive Behaviour: if find another robot run from it");
+    return -1;
+}
+}
+
+public class NormalBehaviour : IBehaviour
+{
+
+    public int moveCommand()
+{
+    Console.WriteLine("\tNormal Behaviour: if find another robot ignore it");
+    return 0;
+}
+}
+
+public class Robot
+{
+    IBehaviour? behaviour;
+    string? name;
+
+    public Robot(string name)
+    {
+        this.name = name;
+    }
+
+    public void setBehaviour(IBehaviour behaviour)
+    {
+        this.behaviour = behaviour;
+    }
+
+    public IBehaviour getBehaviour()
+    {
+        return behaviour;
+    }
+
+    public void move()
+    {
+        Console.WriteLine(this.name + ": Based on current position" +
+                     "the behaviour object decide the next move:");
+        int command = behaviour.moveCommand();
+
+        Console.WriteLine("\tThe result returned by behaviour object " +
+                    "is sent to the movement mechanisms " +
+                    " for the robot '"  + this.name + "'");
+    }
+
+    public string? getName()
+    {
+        return name;
+    }
+
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+}
+
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+        Robot r1 = new Robot("Big Robot");
+        Robot r2 = new Robot("George v.2.1");
+        Robot r3 = new Robot("R2");
+
+        r1.setBehaviour(new AgressiveBehaviour());
+        r2.setBehaviour(new DefensiveBehaviour());
+        r3.setBehaviour(new NormalBehaviour());
+
+        r1.move();
+        r2.move();
+        r3.move();
+
+        Console.WriteLine("\r\nNew behaviours: " +
+                "\r\n\t'Big Robot' gets really scared" +
+                "\r\n\t, 'George v.2.1' becomes really mad because" +
+                "it's always attacked by other robots" +
+                "\r\n\t and R2 keeps its calm\r\n");
+
+        r1.setBehaviour(new DefensiveBehaviour());
+        r2.setBehaviour(new AgressiveBehaviour());
+
+        r1.move();
+        r2.move();
+        r3.move();
+    }
+}
